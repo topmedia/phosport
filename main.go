cfff933ca08eea5d79e5d80ccd18fe2587772c39
Patrick Lenz <patrick@limited-overload.de>
2015-05-13 11:38:40 +0200

    Parsing rules into structures
    Started work on service and host ref resolution

--- main.go
+++ main.go
@@ -1,23 +1,61 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"os/exec"
+	"regexp"
 	"strings"
 )
 
-type Packetfilter struct {
-	Action       string
-	Comment      string
-	Destinations []string
-	Group        string
-	Interface    string
-	Name         string
-	Services     []string
-	Sources      []string
-	Status       bool
+type Rule struct {
+	Data RuleData `json:"data"`
+}
+
+func (r *Rule) ResolveRefs() {
+	// for src := range r.Data.Sources {
+	// }
+	// for dst := range r.Data.Destinations {
+	// }
+	// for svc := range r.Data.Services {
+	// }
+}
+
+type RuleData struct {
+	Action       string   `json:"action"`
+	Comment      string   `json:"comment"`
+	Destinations []string `json:"destinations"`
+	Group        string   `json:"group"`
+	Interface    string   `json:"interface"`
+	Name         string   `json:"name"`
+	Services     []string `json:"services"`
+	Sources      []string `json:"sources"`
+	Status       int      `json:"status"`
+}
+
+type Host struct {
+	Data  HostData `json:"data"`
+	Class string   `json:"class"`
+}
+
+type HostData struct {
+	Address string `json:"address"`
+	Comment string `json:"comment"`
+	Name    string `json:"name"`
+}
+
+type Service struct {
+	Data  ServiceData `json:"data"`
+	Class string      `json:"class"`
+}
+
+type ServiceData struct {
+	DstHigh int    `json:"dst_high"`
+	DstLow  int    `json:"dst_low"`
+	Comment string `json:"comment"`
+	Name    string `json:"name"`
 }
 
 var (
@@ -25,23 +63,46 @@ var (
 		"UTM Hostname")
 )
 
-func ToJSON(input string) string {
+func ToJSON(input []byte) []byte {
+	vars := regexp.MustCompile(`(\$VAR[^,]+),`)
+	fixquotes := func(m string) string {
+		return fmt.Sprintf(`["%s"],`,
+			strings.Replace(m, `"`, "'", -1))
+	}
 
-	input = strings.Replace(input, "'", `"`, -1)
-	input = strings.Replace(input, " => ", ": ", -1)
-	return input
+	str := strings.Replace(string(input), " => ", ": ", -1)
+	str = strings.Replace(str, "'", `"`, -1)
+	str = vars.ReplaceAllStringFunc(str, fixquotes)
+	return []byte(str)
+}
+
+func ConfdCommand(cmd string) []byte {
+	log.Printf("Executing command %s on host %s", cmd, *host)
+	out, err := exec.Command("ssh", *host, "confd-client.plx",
+		cmd).Output()
+
+	if err != nil {
+		log.Fatalf("Error executing confd command: %s", err)
+	}
+
+	return out
 }
 
 func main() {
 	flag.Parse()
 
-	pkf, err := exec.Command("ssh", *host, "confd-client.plx",
-		`get_objects_filtered '$_->{type} eq "packetfilter"'`).
-		Output()
+	pkf := ConfdCommand(`get_objects_filtered '$_->{type} eq "packetfilter"'`)
 
+	var rules []Rule
+	err := json.Unmarshal(ToJSON(pkf), &rules)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error parsing rules into JSON: %s", err)
 	}
 
-	fmt.Println(ToJSON(string(pkf)))
+	for _, rule := range rules {
+		log.Printf("Source: %v | Dest: %v | Services: %v", rule.Data.Sources, rule.Data.Destinations, rule.Data.Services)
+	}
+
+	// log.Printf("Rules: %#v", rules)
+
 }
